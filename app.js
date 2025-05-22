9af80265bd941eee36fe0801034acd01f8cac4ce
Heitor <fernandesheitort2@gmail.com>
2025-05-22 20:55:51 -0300

    sggrherhrejtrj

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function exibirTextoNaTela(tag, texto) {
 }
 
 function exibirMensagemInicial() {
-    exibirTextoNaTela('h1', 'Jogo doo número secreto');
+    exibirTextoNaTela('h1', 'Jogo dos número secreto');
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 }
 
